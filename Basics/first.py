df1711a8767048ae07d6ac02161fbc4053879f52
Priya Sah <rupadevi9131@gmail.com>
2025-06-02 23:06:34 +0530

    Update first.py

--- Basics/first.py
+++ Basics/first.py
@@ -1,6 +1,28 @@
-## to make series from data
-import pandas as pd
-data=[1,2,3,4]
-series=pd.Series(data)
-print("series:\n",series)
-print(type(series))
+##SYntax:
+'''Syntax refers to the set of rules that define the combination of symbol that are considered to be correctly structured program in a language.
+In simpler terms syntax is about the correct arrangement of words and symbols in a code'''
+## basic syntax in python
+''' multi line comment
+'''
+
+##Case sensitivity
+name="priya"
+Name="priyu"
+print(name)
+print(Name)
+
+##indentation
+age=46
+if age>45:
+    print(age)
+
+
+##Line contribution
+total=1+2+3+\
+4+6
+print(total)
+
+##Multiple statements
+x=8;y=8;z=x+y
+print(z)
+
